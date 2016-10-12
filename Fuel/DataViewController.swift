dd54bc0f1bb01b1a49e8769e93e72ec15ec0f692
Mateusz Data <mateusz_data@icloud.com>
2016-10-12 14:16:04 +0200

    Initial Commit

---
+++ Fuel/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Fuel
+//
+//  Created by Mateusz Data on 12.10.2016.
+//  Copyright © 2016 Mateusz Data. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
